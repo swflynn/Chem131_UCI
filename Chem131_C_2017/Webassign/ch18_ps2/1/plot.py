83661aeabaaf486112f48afee88f1b3468db94ca
swflynn <swflynn@uci.edu>
2017-04-11 18:49:49 -0700

    Plot T, C(T) data. Python Code. See myfig.png

--- Chem131_C_2017/Webassign/ch18_ps2/1/plot.py
+++ Chem131_C_2017/Webassign/ch18_ps2/1/plot.py
@@ -1,14 +1,19 @@
+# See prob1.f90 for the beginning of this problem.
+
+# This is a python program to take our data in data.dat and plot it. 
+# Our data file has 2 columns T and C_v
+
+#======================Tell Python to let us make a Graph======================!
 import matplotlib.pyplot as plt        
 
-# This script plots x,y data in a file seperated by a space
-
-with open('data.dat') as f:             #open data file to plot
-    lines = f.readlines()               # read in line by line
+with open('data.dat') as f:         #open data file to get our numbers in python
+    lines = f.readlines()           # read in the data line by line
     x = [line.split()[0] for line in lines]         #x = 1st number
     y = [line.split()[1] for line in lines]         #y = 2nd number
 
-    plt.plot(x,y)                       # plot it
+#===========================Plot the Data===========================!
+    plt.plot(x,y)                       
     plt.xlabel('T(K)')
     plt.ylabel('C_v / R')
     plt.title ('Webassign(2) Ch.18 #1')
-    plt.savefig('myfig.png')                          #save plot to a file
+    plt.savefig('myfig.png')                #save plot to a file myfig.png
