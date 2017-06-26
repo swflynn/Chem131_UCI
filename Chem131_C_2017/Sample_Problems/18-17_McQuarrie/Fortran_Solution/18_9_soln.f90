7da72a20bfcd80a454d7b093d4067ca578503e71
swflynn <swflynn@uci.edu>
2017-06-26 16:46:54 -0700

    Fortran Solution to McQuarrie 18-17

--- Chem131_C_2017/Sample_Problems/18-17_McQuarrie/Fortran_Solution/18_9_soln.f90
+++ Chem131_C_2017/Sample_Problems/18-17_McQuarrie/Fortran_Solution/18_9_soln.f90
@@ -1,73 +1,82 @@
-!This is a Fortran 90 program (.f90), comments are specified with ! and are not part of the code 
+! This problem is similar to Problem 18-17 in McQuarrie. 
 
 !====================================Question=======================================!
-! Plot the vibrational contribution to molar heat capcity of Br2(g) from (250-1000)K
+! Plot the fraction of HBr(g) molecules in various rotation levels (J=0,50)
+! Compare T = 300k and T = 1000K population distributions
 !====================================Question=======================================!
 
 !====================================Solution=======================================!
-! From P.741 of McQuarrie : C = R(O/T)^2 (e^-o/T / (1-e^-o/T)^2)
+! From P.745 of McQuarrie : f(J) = (2J+1) (o/T) (e^-o*J(J+1)/T)
+! Expect at higher Temperature we have more energy, therefore higher J states 
+! Have a higher probability of being populated. 
 !====================================Solution=======================================!
 
 !====================================Program=======================================!
-! Simple program to calculate C/R as a function of T from 250-1000 K
-! The data is written to a file data.dat
-! See plot.py Python code for plotting this data file
+! Set our indepedent variable J (quantum number) from 0-50: (j)
+! Solve for population density at 300K: (f)
+! Solve for population density at 1000K: (g)
+! Write data to a file for plotting in Python
 !====================================Program=======================================!
 
-PROGRAM ch18_9                            ! Name your program
+PROGRAM MQ_18_17
+  IMPLICIT NONE
+  REAL, PARAMETER :: rot_temp=12.02       ! McQuarrrie P. 739
+  REAL, PARAMETER :: T=300              
+  REAL, PARAMETER :: T2=1000
+  Real, Parameter :: increment=1          !Evaluate at every whole number
+  Integer, Parameter :: n = (50-0)/increment
+  REAL, DIMENSION(1:n) :: j, f, g         !Data to Plot, J and the 2 different T
+  INTEGER :: i                            ! Looping Variable
 
-!Next we define all our variables 
+! Always start by initializing variables we want to calculate (set them to 0)
+  f = 0
+  j = 0
+  g = 0
 
-IMPLICIT NONE                           ! Statement Saying I will define the variables myself
-REAL, PARAMETER :: vib_temp = 463.      ! McQuarrie P. 739
-REAL, PARAMETER :: increment = 1        ! Calculate for each whole number
-INTEGER, PARAMETER:: n = (1001-250)/increment ! Number of Evaluations we make
-INTEGER :: i                            !Looping Variable
-REAL, DIMENSION(1:n) :: T, C_v          ! Our Data: Temperature and Heat Capacity
-
-!================================Data Types=======================================!
-! Real = anything with a decimal
-! integer = whole numbers
-! Dimension = vector or matrix (a list of numbers is a vector) so a column of data  
-! Parameter = value that cannot be changed during the code
-!================================Data Types=======================================!
-  
-! initialize all variables we are solving for (make sure they start as 0)
-  T = 0
-  C_v = 0
-
-! Set our initial temperature (indepedent variable) and fill a vector with the values. 
-  T(1) = 250.                 ! T is a vector (a column of data), the first value is 250
-  DO i =2,n
-    T(i) = T(i-1) + increment
+! Calculate from J = 0 to J = 50 (indepedent variable). 
+  j(1) = 0                    ! the first element of vector j is 0
+  DO i =2,n                   ! fill the remainder of the elements
+    j(i) = j(i-1) + increment
   END DO
 
-! Now solve for heat capacity  using our function defined below
+! Use function below to calculate population density at 300K
   DO i = 1, n
-    C_v(i) = heat_capacity(T(i))  ! For each T(i) evaluate C(T)
+    f(i) = population(j(i))
+  END DO
+  
+! Use function below to calculate population density at 1000K
+  DO i = 1, n
+    g(i) = population2(j(i))
   END DO
 
-! Write our data to a file called data.dat:  T, C
+!Write our data to a file called data.dat  j  f(j) at 300K  f(j) at 1000K
   OPEN (UNIT =1, FILE='data.dat')
   DO i=1,n
-    WRITE(1,*) T(i), C_v(i)
+    WRITE(1,*) j(i), f(i), g(i)
   END DO
   CLOSE (UNIT=1)
 
-! Here we define our function; we want to take a value of T and calculate C_v
-! Our function takes in any value (x) and returns a number (values)
-! Take in a temperature and use the equation to calculate heat capacity at that temperature
+!Want a function that takes in a value of J and gives a population density f(J)
+  CONTAINS 
 
-  CONTAINS                ! statement for adding a function of subroutine
-    
-    FUNCTION heat_capacity(x) RESULT (values)
+    FUNCTION population(x) RESULT (values)
       IMPLICIT NONE
       REAL :: x, values
       
-      values = (vib_temp / x)**2 * ((EXP(-(vib_temp / x))) / ((1. - EXP(-(vib_temp / x)))**2))
+      values = (2.*x +1.) * (rot_temp / T) * (EXP(-(rot_temp*x*(x+1))/T))
 
-    END FUNCTION heat_capacity
+    END FUNCTION population
+    
+    FUNCTION population2(x) RESULT (values)
+      IMPLICIT NONE
+      REAL :: x, values
+      
+      values = (2.*x +1.) * (rot_temp / T2) * (EXP(-(rot_temp*x*(x+1))/T2))
 
-! I will plot the data written to data.dat using a python script see plot.py code
+    END FUNCTION population2
+! There is a better way to do this, but for simplicity I just write two funcitons 
+! One for each temperature we are asked to evaluate.
 
-END PROGRAM ch18_9
+! I plot the data using a python script, see plot.py and myfig.png
+
+END PROGRAM MQ_18_17
